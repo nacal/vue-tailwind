7a3adc6b83a6783f7f52734d42faafe4003986bd
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-01-22 22:10:32 +0900

    add PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,15 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.vue'
+  ],
+  defaultExtractor: (content) => {
+    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+  },
+  whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
+})
 
 module.exports = {
   plugins: [
